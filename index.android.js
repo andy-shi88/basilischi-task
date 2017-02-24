98191f9d1bc6d38cb473524d901c15943564bcd1
andy-shi88 <shi88.andy@gmail.com>
2017-02-24 12:35:10 +0700

    Basilischi react-native task-1 commit

---
+++ index.android.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class BasilischiTask extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+         <Image source={require('./test.jpg')} style={{width: 400, height: 300}}/>
+        <Text style={styles.welcome}>
+          Andy Herman - basilischi_batch
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('BasilischiTask', () => BasilischiTask);
